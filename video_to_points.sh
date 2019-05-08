70685d7e9683a1f43bdd7733f504420e03a9e65c
asterios <asterios969@gmail.com>
2019-05-08 23:42:48 +0300

    Added bash video_to_points

---
+++ video_to_points.sh
@@ -0,0 +1,40 @@
+$0 # the path to the current file
+$1 # inputname
+$2 # outputname
+$3 # fps 10 or 25
+$4 # rotchoice
+mkdir -p $2/rgb
+
+if [ $4 = "n" ] || [ $4 = "N" ]
+then
+ffmpeg -i $1 -r $3 -vf scale=-1:320 $2/rgb/img%04d.png
+
+elif [ $4 = "y" ] || [ $4 = "Y" ]
+then
+ffmpeg -i $1 -r $3 -vf scale=320:-1,"transpose=1" $2/rgb/img%04d.png
+
+else
+    echo "Invalid choice. Choose y/n"
+    echo
+    exit 1
+fi
+
+#Counts the number of output files
+imgnum=$(ls $2/rgb | wc -l)
+
+echo "# colour images" > $2/rgb.txt
+echo "#file: '$outputname'" >> $2/rgb.txt
+echo "# timestamp filename" >> $2/rgb.txt
+
+#Uses bc to calculate timestamp increment to 6 places
+#No spaces around =
+frameTime=$(bc <<< "scale=6; 1.0/$fps")
+timestamp=0.000000
+
+for i in $(seq -f "%04g" $imgnum)
+do
+echo $timestamp rgb/img$i.png >> $2/rgb.txt
+timestamp=$(bc <<< "scale=6; $timestamp+$frameTime")
+done
+
+mv $1 $2
