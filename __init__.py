4586bf21162c99a32015a323ab9fb584ce03f9a1
asterios <asterios969@gmail.com>
2019-05-15 01:24:37 +0300

    Created base logic for exporting settings to yaml file

--- __init__.py
+++ __init__.py
@@ -355,9 +355,29 @@ class WM_OT_export_settings(bpy.types.Operator):
     bl_idname = "wm.export_settings"
     bl_label = "Export Settings"
 
+    bl_options = {'REGISTER'}
+
+    # Define this to tell 'fileselect_add' that we want a directoy
+    directory = bpy.props.StringProperty(
+        name="Outdir Path",
+        description="Where I will save my stuff"
+        # subtype='DIR_PATH' is not needed to specify the selection mode.
+        # But this will be anyway a directory path.
+        )
+
     def execute(self, context):
-        # TODO
+
+        print("Selected dir: '" + self.directory + "'")
+
         return {'FINISHED'}
+
+    def invoke(self, context, event):
+        # Open browser, take reference to 'self' read the path to selected
+        # file, put path in predetermined self fields.
+        # See: https://docs.blender.org/api/current/bpy.types.WindowManager.html#bpy.types.WindowManager.fileselect_add
+        context.window_manager.fileselect_add(self)
+        # Tells Blender to hang on for the slow user input
+        return {'RUNNING_MODAL'}
 
 class WM_OT_apply_settings(bpy.types.Operator):
     bl_idname = "wm.apply_settings"
